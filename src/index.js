695d216b43a80f591a208ea89454dd4e30c90f6d
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-02-18 15:43:05 +0200

    fix/togleArticle, delete comments

--- src/index.js
+++ src/index.js
@@ -13,18 +13,6 @@
     const elementArticle = document.querySelector('.article');
     const isShow = elementButton.getAttribute('data-value');
     console.log(isShow);
-    // console.log(elementArticle.getAttribute('length'));
-    // debugger;
-    // if (isShow) {
-    //   elementArticle.setAttribute('length', '100');
-    // } else {
-    //   debugger;
-    //   elementArticle.setAttribute('length', $rootScope.article.length);
-    // }
-    // debugger
-    // isShow ? elementButton.setAttribute('data-value', 'false') : elementButton.setAttribute('data-value', 'true');
-    // console.log(document.querySelector('.button-article').getAttribute('data-value'));
-    // console.log(document.querySelector('.article'));
   };
 
   $rootScope.article = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, impedit. Recusandae sequi eum impedit quis delectus eaque molestiae magni illo quaerat sit quos, enim rerum assumenda voluptas nesciunt iste iure dolorum et aliquid natus repudiandae non. Asperiores excepturi fuga aspernatur nulla provident, enim placeat minus illo corporis soluta laudantium est commodi minima suscipit reiciendis temporibus molestias ut rerum voluptatibus eum id doloribus accusantium? Explicabo cumque vel sequi placeat qui quidem voluptate distinctio tempore dolorem rem! Soluta esse sequi in magnam eius autem? Deleniti velit itaque voluptate nemo architecto delectus, labore dolores harum. Numquam aliquid neque id a exercitationem atque perferendis veniam culpa ducimus. Dolorem quia, magnam repellat maiores nulla tempora delectus rem aliquid voluptas voluptate, doloremque accusamus optio? Iusto tenetur autem atque mollitia quis aliquam, quod incidunt minima alias beatae dolore praesentium? Porro voluptatibus facere repellat architecto maiores. Blanditiis repellat, perspiciatis, itaque et eveniet reprehenderit quisquam repudiandae similique vitae doloremque voluptates enim quia maxime nam minima voluptatibus beatae quasi. Qui suscipit tempore repudiandae quis veritatis voluptatem. Vitae, distinctio at odit ea unde iste deserunt quisquam optio perferendis. Quos ducimus dolores molestiae dolorem. Repellat voluptatem nemo officiis, illum numquam vel. Tenetur numquam labore esse dolores similique, sed repudiandae corrupti pariatur fugiat blanditiis voluptatibus consequuntur quos quas at alias est quod fugit explicabo perspiciatis iusto nobis sapiente fuga. Quisquam perspiciatis illo deleniti explicabo totam ipsum, earum vel, obcaecati magnam repellendus quidem, accusamus id minima! Necessitatibus doloremque repudiandae cumque adipisci, sed atque nisi dolorum facilis sunt excepturi repellendus non eligendi a illum ad nemo libero reprehenderit, reiciendis mollitia, eum magni suscipit! Perferendis consequatur, consequuntur ab itaque cumque accusantium debitis error velit. Iusto dicta nemo animi omnis necessitatibus delectus aliquam libero adipisci harum soluta quibusdam, sapiente aut commodi ipsam exercitationem ratione incidunt hic culpa aliquid, quasi corrupti maxime at voluptatum. Corporis minus necessitatibus veritatis.';
